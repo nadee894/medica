bfe542c304f0d8b792fd00633b6e0645ea01d581
Nadeesha Liyanage <nadlyng894@gmail.com>
2016-01-23 08:50:27 +0530

    Login Interface
    Login Interface

--- Medica/Form1.Designer.vb
+++ Medica/Form1.Designer.vb
@@ -22,9 +22,98 @@ Partial Class Form1
     'Non modificarla nell'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblUserName = New System.Windows.Forms.Label()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogIn = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnForgotPassword = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblUserName
+        '
+        Me.lblUserName.AutoSize = True
+        Me.lblUserName.Location = New System.Drawing.Point(93, 22)
+        Me.lblUserName.Name = "lblUserName"
+        Me.lblUserName.Size = New System.Drawing.Size(57, 13)
+        Me.lblUserName.TabIndex = 0
+        Me.lblUserName.Text = "UserName"
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(96, 50)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(148, 20)
+        Me.txtUserName.TabIndex = 1
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(93, 87)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(53, 13)
+        Me.lblPassword.TabIndex = 2
+        Me.lblPassword.Text = "Password"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(96, 119)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(148, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'btnLogIn
+        '
+        Me.btnLogIn.Location = New System.Drawing.Point(27, 162)
+        Me.btnLogIn.Name = "btnLogIn"
+        Me.btnLogIn.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogIn.TabIndex = 4
+        Me.btnLogIn.Text = "Login"
+        Me.btnLogIn.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(121, 162)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 5
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnForgotPassword
+        '
+        Me.btnForgotPassword.Location = New System.Drawing.Point(215, 162)
+        Me.btnForgotPassword.Name = "btnForgotPassword"
+        Me.btnForgotPassword.Size = New System.Drawing.Size(115, 23)
+        Me.btnForgotPassword.TabIndex = 6
+        Me.btnForgotPassword.Text = "Forgotton Password"
+        Me.btnForgotPassword.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(344, 208)
+        Me.Controls.Add(Me.btnForgotPassword)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnLogIn)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.lblUserName)
+        Me.Name = "Form1"
+        Me.Text = "Login Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents lblUserName As System.Windows.Forms.Label
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents lblPassword As System.Windows.Forms.Label
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogIn As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents btnForgotPassword As System.Windows.Forms.Button
 
 End Class
