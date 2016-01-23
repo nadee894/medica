48301ba1db290253df04b4a79e16911e91539147
Nadeesha Liyanage <nadlyng894@gmail.com>
2016-01-23 08:01:33 +0530

    Initiating project
    Initiating project

---
+++ Medica/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' insieme di attributi. Per modificare le informazioni associate a un assembly
+' è necessario modificare i valori di questi attributi.
+
+' Controllare i valori degli attributi dell'assembly
+
+<Assembly: AssemblyTitle("Medica")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Medica")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+<Assembly: Guid("53a0f247-4c99-447d-8cc1-03a30bd4f9e5")> 
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Numero di versione principale
+'      Numero di versione secondario 
+'      Numero build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
+' utilizzando l'asterisco (*) come descritto di seguito:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
